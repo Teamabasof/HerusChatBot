ac5d2b54a46f3ff15909d7c6c610283bcd16bad2
SakirBey1 <sakirhack81@gmail.com>
2022-04-18 00:27:35 +0300

    Update herlock.py

--- herlock.py
+++ herlock.py
@@ -50,7 +50,7 @@ async def type_and_send(message):
     await message._client.send_chat_action(chat_id, "cancel")
 
 
-@herlock.on_message(filters.command("repo") & ~filters.edited)
+@luna.on_message(filters.command("repo") & ~filters.edited)
 async def repo(_, message):
     await message.reply_text(
         "[GitHub](https://instagram.com/developer.rat)"
